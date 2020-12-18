4bef94d5a6dbe0c95563129aed358df28486cc65
Francis9817 <61259614+Francis9817@users.noreply.github.com>
2020-12-18 00:26:38 -0500

    Add files via upload

---
+++ Excepciones/Excepciones.Net/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Excepciones.Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Borracho oBorracho = new Borracho() { Nombre = "Pepito", NumeroDeCervezasQueHaTomado = 0 };
+                oBorracho.NumeroDeCervezasQueHaTomado = 14;
+
+                if (oBorracho.NumeroDeCervezasQueHaTomado >= 10)
+                {
+                    throw new Exception("Ocurrio un desastre");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("El borracho salio del bar");
+            }     
+        }
+        
+        
+        public class Borracho
+        {
+            public string Nombre { get; set; }
+            public int NumeroDeCervezasQueHaTomado { get; set; }
+
+        }
+
+    }
+
+
+}
